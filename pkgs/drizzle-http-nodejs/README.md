997fbbd2f39f0a1169dc2e6d781d9006b01f62d0
Vitor Hugo Salgado <vsalgadopb@gmail.com>
2021-02-14 20:42:07 -0300

    feat: add package for nodejs basic imports

---
+++ pkgs/drizzle-http-nodejs/README.md
@@ -0,0 +1,9 @@
+# Drizzle-HTTP Node.js
+
+Basic packages for Node.js
+
+## Installation
+
+```
+npm i @drizzle-http/nodejs
+```
---
+++ pkgs/drizzle-http-nodejs/src/index.ts
@@ -0,0 +1,3 @@
+export * from '@drizzle-http/core'
+export * from '@drizzle-http/undici'
+export * from '@drizzle-http/logging-interceptor'
