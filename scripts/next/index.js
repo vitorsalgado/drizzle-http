7d4a3accacb1f29ba9a447b7abea933bae3539d5
Vitor Hugo Salgado <vsalgadopb@gmail.com>
2021-02-23 02:04:21 -0300

    fix: name in next val script

--- scripts/next/index.js
+++ scripts/next/index.js
@@ -4,10 +4,10 @@ const checkChanges = () => {
   const changed = JSON.parse(execSync('lerna changed --json').toString())
 
   if (changed.length === 0) {
-    console.warn('Found no changes for canary release.')
+    console.warn('Found no changes for next release.')
     process.exit(1)
   } else {
-    console.log(`Found ${changed.length} packages for canary release.`)
+    console.log(`Found ${changed.length} packages for next release.`)
   }
 }
 
