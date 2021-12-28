0d322e3a547bb613a7f6c6b205bb2bbb424f0491
Vitor Hugo Salgado <vsalgadopb@gmail.com>
2021-12-27 23:56:20 -0300

    refactor(all): remove fetch dep + add other nodejs modules

--- pkgs/drizzle-http/__tests__/all.spec.ts
+++ pkgs/drizzle-http/__tests__/all.spec.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import EventEmitter from 'events'
 import { TestId, TestResult } from '@drizzle-http/test-utils'
 import { noop } from '@drizzle-http/core'
--- pkgs/drizzle-http/index.ts
+++ pkgs/drizzle-http/index.ts
@@ -1,5 +1,6 @@
 export * from '@drizzle-http/core'
 export * from '@drizzle-http/undici'
-export * from '@drizzle-http/fetch'
 export * from '@drizzle-http/logging-interceptor'
 export * from '@drizzle-http/rxjs-adapter'
+export * from '@drizzle-http/response-mapper-adapter'
+export * from '@drizzle-http/opossum-circuit-breaker'
